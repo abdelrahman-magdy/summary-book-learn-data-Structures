f6535d71bc583fd3fb0bc609e9de782ea3f36bd5
AbdElrahman <abdomagdyk@gmail.com>
2021-03-22 11:35:01 +0200

    add graph & stack algrothems

--- summary book-learn-data-Structures.js
+++ summary book-learn-data-Structures.js
@@ -1,5 +1,106 @@
-//  LinkedList
+function Stack() { //properties and methods go here
+    let items = [];
 
+    this.push = function (element) {
+        items.push(element);
+    };
+
+    this.pop = function () {
+        return items.pop();
+    };
+
+    this.peek = function () {
+        return items[items.length - 1];
+    };
+
+    this.isEmpty = function () {
+        return items.length == 0;
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.clear = function () {
+        items = [];
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
+
+}
+
+function TESTsTACK(params) {
+
+    let stack = new Stack();
+
+    stack.isEmpty()
+
+    stack.push('one')
+
+    stack.push('two')
+
+    stack.push('three')
+
+    stack.peek()
+
+    stack.size()
+
+    stack.print()
+
+    stack.isEmpty()
+
+    stack.pop()
+
+    stack.pop()
+
+    stack.size()
+}
+
+function divideBy2(decNumber) {
+
+    var remStack = new Stack(),
+        rem,
+        binaryString = '';
+
+    while (decNumber > 0) { //{1}
+        rem = Math.floor(decNumber % 2); //{2}
+        remStack.push(rem); //{3}
+        decNumber = Math.floor(decNumber / 2); //{4}
+    }
+
+    while (!remStack.isEmpty()) { //{5}
+        binaryString += remStack.pop().toString();
+    }
+
+    return binaryString;
+}
+
+function baseConverter(decNumber, base) {
+
+    var remStack = new Stack(),
+        rem,
+        baseString = '',
+        digits = '0123456789ABCDEF'; //{6}
+    while (decNumber > 0) {
+        rem = Math.floor(decNumber % base);
+        remStack.push(rem);
+        decNumber = Math.floor(decNumber / base);
+    }
+    while (!remStack.isEmpty()) {
+        baseString += digits[remStack.pop()]; //{7}
+    }
+    return baseString;
+    
+}
+
+
+
+
+
+
+//  LinkedList
 function LinkedList() {
     let Node = function (element) {
         this.element = element;
@@ -513,6 +614,9 @@ function HashTable_3() {
         return undefined; //{14}
     };
 }
+
+
+// tree
 // search binary Tree
 function BinarySearchTree() {
 
@@ -991,4 +1095,99 @@ x = {
         'left': null
     }
 
+}
+// end tree
+
+// Graph
+function Graph() {
+
+    var vertices = []; //{1}
+    var adjList = new Dictionary(); //{2}
+
+    this.addVertex = function (v) {
+        vertices.push(v); //{3}
+        adjList.set(v, []); //{4}
+    };
+
+    this.addEdge = function (v, w) {
+        adjList.get(v).push(w); //{5}
+        adjList.get(w).push(v); //{6}
+    };
+
+    this.toString = function () {
+        var s = '';
+        for (var i = 0; i < vertices.length; i++) { //{10}
+            s += vertices[i] + ' -> ';
+            var neighbors = adjList.get(vertices[i]); //{11}
+            for (var j = 0; j < neighbors.length; j++) { //{12}
+                s += neighbors[j] + ' ';
+            }
+            s += '\n'; //{13}
+        }
+        return s;
+    };
+    // graph traversals
+    // -> breadth-first-search (bfs)
+    // -> depth-first-search (DFS)
+
+    var initializeColor = function () {
+        var color = [];
+        for (var i = 0; i < vertices.length; i++) {
+            color[vertices[i]] = 'white'; //{1}
+        }
+        return color;
+    };
+
+    this.bfs = function (v, callback) {
+        var color = initializeColor(), //{2}
+            queue = new Queue(); //{3}
+        queue.enqueue(v); //{4}
+        while (!queue.isEmpty()) { //{5}
+            var u = queue.dequeue(), //{6}
+                neighbors = adjList.get(u); //{7}
+            color[u] = 'grey'; //{8}
+            for (var i = 0; i < neighbors.length; i++) { //{9}
+                var w = neighbors[i]; //{10}
+                if (color[w] === 'white') { //{11}
+                    color[w] = 'grey'; //{12}
+                    queue.enqueue(w); //{13}
+                }
+            }
+            color[u] = 'black'; //{14}
+            if (callback) { //{15}
+                callback(u);
+            }
+        }
+    };
+}
+
+// test
+function testGraph() {
+
+    var graph = new Graph();
+    var myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; //{7}
+    for (var i = 0; i < myVertices.length; i++) { //{8}
+        graph.addVertex(myVertices[i]);
+    }
+    graph.addEdge('A', 'B'); //{9}
+    graph.addEdge('A', 'C');
+    graph.addEdge('A', 'D');
+    graph.addEdge('C', 'D');
+    graph.addEdge('C', 'G');
+    graph.addEdge('D', 'G');
+    graph.addEdge('D', 'H');
+    graph.addEdge('B', 'E');
+    graph.addEdge('B', 'F');
+    graph.addEdge('E', 'I');
+
+    console.log(graph.toString());
+    // A -> B C D 
+    // B -> A E F 
+    // C -> A D G 
+    // D -> A C G H 
+    // E -> B I 
+    // F -> B 
+    // G -> C D 
+    // H -> D 
+    // I -> E
 }
