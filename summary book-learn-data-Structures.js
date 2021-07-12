63802cd0f0f188c37be38fcb0a381d70117ea163
AbdElrahman <abdomagdyk@gmail.com>
2021-07-12 21:21:36 +0200

    add Queue and implement it

--- summary book-learn-data-Structures.js
+++ summary book-learn-data-Structures.js
@@ -96,8 +96,86 @@ function baseConverter(decNumber, base) {
 }
 
 
+function Queue() {
+    let items = [];
+    //properties and methods go here
+    this.enqueue = function (element) {
+        items.push(element);
+    }
 
+    this.dequeue = function () {
+        return items.shift();
+    };
 
+    this.front = function () {
+        return items[0];
+    };
+
+    this.isEmpty = function () {
+        return items.length == 0;
+    };
+
+    this.size = function () {
+        return items.length;
+    };
+
+    this.print = function () {
+        console.log(items.toString());
+    };
+}
+let Queue2 = (function () {
+    const items = new WeakMap();
+    class Queue2 {
+        constructor() {
+            items.set(this, []);
+        }
+        enqueue(element) {
+            let q = items.get(this);
+            q.push(element);
+        }
+        dequeue() {
+            let q = items.get(this);
+            let r = q.shift();
+            return r;
+        }
+        //other methods
+    }
+    return Queue2;
+})();
+
+function implmentQueue() {
+    let queue = new Queue();
+    // console.log(queue.isEmpty()); //outputs true
+}
+
+function PriorityQueue() {
+    let items = [];
+
+    function QueueElement(element, priority) { // {1}
+        this.element = element;
+        this.priority = priority;
+    }
+    this.enqueue = function (element, priority) {
+        let queueElement = new QueueElement(element, priority);
+        let added = false;
+        for (let i = 0; i < items.length; i++) {
+            if (queueElement.priority < items[i].priority) { // {2}
+                items.splice(i, 0, queueElement); // {3}
+                added = true;
+                break; // {4}
+            }
+        }
+        if (!added) {
+            items.push(queueElement); //{5}
+        }
+    };
+    this.print = function () {
+        for (let i = 0; i < items.length; i++) {
+            console.log(`${items[i].element} -
+        ${items[i].priority}`);
+        }
+    };
+}
 
 
 //  LinkedList
